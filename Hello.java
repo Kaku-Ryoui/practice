c37b58ad88d4a2eb99fa3ca4f2a7e335a371275c
kakun <mlis_1112@yahoo.co.jp>
2021-10-19 00:57:16 +0900

    ファイルへの書き込み

--- Hello.java
+++ Hello.java
@@ -1,6 +1,20 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello!");
         System.out.println("test");
+        try {
+        	File file = new File("C:\\Users\\KK\\test.txt");//これだけだとfileは作成されない
+        	FileWriter filewriter = new FileWriter(file);
+        	filewriter.write("Hello！World");
+        	
+        	filewriter.close();
+        }catch(IOException e){
+        	System.out.println(e);
+        }
+        
     }
 }
