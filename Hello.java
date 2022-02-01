b386baf505a20e71cc63a2eb8bd91767f4d032e5
kakun <mlis_1112@yahoo.co.jp>
2022-02-02 00:21:08 +0900

    ログ記載

--- Hello.java
+++ Hello.java
@@ -3,10 +3,12 @@ import java.io.IOException;
 
 public class Hello {
     public static void main(String[] args) {
+    	System.out.println("テストスタート");
         // FileWriterクラスのオブジェクトを生成する
         try(FileWriter filewriter = new FileWriter("C:\\Users\\KK\\test.txt");){
         	filewriter.write("2/1test");
         }catch(IOException e){
         }
+    	System.out.println("テスト終了");
     }
 }
